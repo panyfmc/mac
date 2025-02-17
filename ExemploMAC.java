3a5a0084b998d148e0cc680db5acc18856b7b6e4
panyfmc <paulavanessa010@gmail.com>
2025-02-17 17:54:09 -0300

    :bug: fix: localizar apenas o endereço mac da rede ativa no dispositivo

--- ExemploMAC.java
+++ ExemploMAC.java
@@ -2,22 +2,29 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-public class ExemploMAC{
+public class ExemploMAC {
     public static void main(String[] args) {
         try {
-            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); //declarando variavel com instancia
-                // interfaces Recebe a lista de todas as interfaces de rede disponíveis no sistema/
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 
-            while (interfaces.hasMoreElements()) {      //se tem mais elementos a serem iterados 
+            while (interfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = interfaces.nextElement();
-                System.out.println("Interface de rede: " + networkInterface.getName());
-                byte[] mac = networkInterface.getHardwareAddress();
-                if (mac != null) {
-                    //System.out.print("Endereço MAC da interface " + networkInterface.getName() + ": ");
-                    for (int i = 0; i < mac.length; i++) {
-                        System.out.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : "");
+
+                // Verifica se a interface está ativa e não é loopback
+                if (networkInterface.isUp() && !networkInterface.isLoopback()) {
+                    byte[] mac = networkInterface.getHardwareAddress();
+
+                    // Verifica se a interface tem um endereço MAC e se o nome sugere que é Wi-Fi
+                    if (mac != null && ((networkInterface.getName().startsWith("wireless")) ||
+                        (networkInterface.getName().startsWith("wlan")) ||
+                        (networkInterface.getName().startsWith("wlp")))) {
+                        System.out.print("Endereço MAC da interface Wi-Fi: ");
+                        for (int i = 0; i < mac.length; i++) {
+                            System.out.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : "");
+                        }
+                        System.out.println();
+                        break; // Sai do loop após encontrar a interface Wi-Fi ativa
                     }
-                    System.out.println();
                 }
             }
         } catch (SocketException e) {
